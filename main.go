0688aec034323f2898d43cb591737d79688f19da
brandaoplaster <brandaoplaster@gmail.com>
2024-03-19 22:57:31 -0300

    feat: Add config db

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"github.com/brandaoplaster/gopportunities/config"
 	"github.com/brandaoplaster/gopportunities/router"
 )
 
 func main() {
-	println("Hello, World!")
+	println("Starting...")
+
+	erro := config.Init()
+
+	if erro != nil {
+		println("Error initializing config")
+		return
+	}
+
 	router.Initialize()
 }
