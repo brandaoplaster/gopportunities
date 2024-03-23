820d734b9bbc51232389904b3ba1ab2578fa7d89
brandaoplaster <brandaoplaster@gmail.com>
2024-03-22 22:20:44 -0300

    feat: Add response

---
+++ handler/response.go
@@ -0,0 +1,8 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func sendError(context *gin.Context, statusCode int, message string) {
+	context.Header("Content-Type", "application/json")
+	context.JSON(statusCode, gin.H{"error": message})
+}
