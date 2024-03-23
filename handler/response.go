7a67b328f9e639a367f01e4dd2fbd51265caf929
brandaoplaster <brandaoplaster@gmail.com>
2024-03-22 22:29:36 -0300

    feat: Add sendSuccess in response

--- handler/response.go
+++ handler/response.go
@@ -1,8 +1,20 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func sendError(context *gin.Context, statusCode int, message string) {
 	context.Header("Content-Type", "application/json")
 	context.JSON(statusCode, gin.H{"error": message})
+}
+
+func sendSuccess(context *gin.Context, message string, data interface{}) {
+	context.Header("Content-Type", "application/json")
+	context.JSON(http.StatusOK, gin.H{
+		"message": message,
+		"data":    data,
+	})
 }
