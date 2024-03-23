3fdd3d90de4681a3546594459fc29372488a0014
brandaoplaster <brandaoplaster@gmail.com>
2024-03-22 22:30:57 -0300

    feat: Add request and validates

--- handler/request.go
+++ handler/request.go
@@ -1,5 +1,7 @@
 package handler
 
+import "fmt"
+
 type CreateOpeningRequest struct {
 	Role     string `json:"role"`
 	Company  string `json:"company"`
@@ -7,4 +9,36 @@ type CreateOpeningRequest struct {
 	Remote   *bool  `json:"remote"`
 	Link     string `json:"link"`
 	Salary   int64  `json:"salary"`
+}
+
+func errorParamsIsRequired(name, valueType string) error {
+	return fmt.Errorf("param %s is required and its type should be %s", name, valueType)
+}
+
+func (r *CreateOpeningRequest) Validate() error {
+	if r.Role == "" {
+		return errorParamsIsRequired("role", "string")
+	}
+
+	if r.Company == "" {
+		return errorParamsIsRequired("company", "string")
+	}
+
+	if r.Location == "" {
+		return errorParamsIsRequired("location", "string")
+	}
+
+	if r.Remote == nil {
+		return errorParamsIsRequired("remote", "bool")
+	}
+
+	if r.Link == "" {
+		return errorParamsIsRequired("link", "string")
+	}
+
+	if r.Salary <= 0 {
+		return errorParamsIsRequired("salary", "int64")
+	}
+
+	return nil
 }
