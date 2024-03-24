723dab89d29bada2c0bccedd646981d4b59ddcff
brandaoplaster <brandaoplaster@gmail.com>
2024-03-23 23:06:56 -0300

    feat: Add struct update opening request

--- handler/request.go
+++ handler/request.go
@@ -11,8 +11,24 @@ type CreateOpeningRequest struct {
 	Salary   int64  `json:"salary"`
 }
 
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
 func errorParamsIsRequired(name, valueType string) error {
 	return fmt.Errorf("param %s is required and its type should be %s", name, valueType)
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+	return fmt.Errorf("at least one field should be updated")
 }
 
 func (r *CreateOpeningRequest) Validate() error {
